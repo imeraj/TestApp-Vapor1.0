6c462c9dbe445eb72399246110e6266efa4cd20d
Mohammad Merajul Islam Molla <merajul.molla@sekai-lab-bd.com>
2016-10-06 15:53:54 +0600

    docker-compose added

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -2,17 +2,8 @@ import Vapor
 
 let drop = Droplet()
 
-// drop.get { req in
-//     let lang = req.headers["Accept-Language"]?.string ?? "en"
-//     return try drop.view.make("welcome", [
-//     	"message": Node.string(drop.localization[lang, "welcome", "title"])
-//     ])
-// }
-
 drop.get("greeting") { req in 
     return "Hello World!"
 }
-
-drop.resource("posts", PostController())
 
 drop.run()
