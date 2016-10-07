e36c443d823ad153df730ab03725264680b30dff
Mohammad Merajul Islam Molla <merajul.molla@sekai-lab-bd.com>
2016-10-07 10:35:48 +0600

    More complete implementation of Sighting model New REST APIs added

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -8,12 +8,31 @@ drop.post("sightings") { request in
         throw Abort.badRequest
     }
     
-    print(request.headers)
+    let response = try drop.client.get("http://ebird.org/ws1.1/ref/taxon/find", query: [
+        "q" : bird ])
+    
+    guard let _ = response.data[0, "name"]?.string else {
+        throw Abort.custom(
+             status: .badRequest,
+             message: "Bird \(bird) was not found")
+    }
     
     var sighting = Sighting(bird: bird)
     try sighting.save()
     
     return sighting
+}
+
+drop.get("sightings", Sighting.self) { request, sighting in
+    return sighting
+}
+
+drop.get("sightings", String.self, "count") { request, bird in
+    let sightings = try Sighting.query().filter("bird", bird).all()
+    
+    return JSON([
+            "count": Node(sightings.count)
+        ])
 }
 
 drop.run()
