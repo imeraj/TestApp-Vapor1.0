96341da58df77b9196a881cafad3ccc7cb1e0dfe
Mohammad Merajul Islam Molla <meraj.enigma@gmail.com>
2016-10-09 10:52:46 +0600

    Add log based on environment

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -3,7 +3,6 @@ import VaporMySQL
 import Foundation
 
 let drop = Droplet(preparations: [Sighting.self], providers: [VaporMySQL.Provider.self])
-
 let sightings = SightingConroller(droplet: drop)
 
 drop.post("sightings", handler: sightings.store)
@@ -11,5 +10,10 @@ drop.post("sightings", handler: sightings.store)
 drop.get("sightings", handler: sightings.index)
 drop.get("sightings", Sighting.self, handler: sightings.show)
 drop.get("sightings", String.self, "count", handler: sightings.count)
+
+if drop.environment == .development {
+    print("API registration done!")
+}
+
 
 drop.run()
