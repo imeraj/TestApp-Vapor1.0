d87657ffb144cd52708012ce8e90c80045df157d
Mohammad Merajul Islam Molla <meraj.enigma@gmail.com>
2016-10-10 11:43:07 +0600

    Added session management tes API

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -4,12 +4,14 @@ import VaporMySQL
 import Foundation
 import SwiftyBeaverVapor
 import SwiftyBeaver
+import Sessions
 
 let console = ConsoleDestination()
 let sbProvider = SwiftyBeaverProvider(destinations: [console])
 
 var middleware: [String: Middleware]? = [
-    "sighting": SightingErrorMiddleware()
+    "sighting": SightingErrorMiddleware(),
+    "sessions" : SessionsMiddleware(sessions: MemorySessions())
 ]
 
 let drop = Droplet(availableMiddleware: middleware, preparations: [Sighting.self], providers: [VaporMySQL.Provider.self], initializedProviders: [sbProvider])
@@ -28,5 +30,23 @@ if drop.environment == .development {
     log.info("API registration done!")
 }
 
+// session test api
+drop.post("remember") { request in
+    guard let name = request.data["name"]?.string else {
+        throw Abort.badRequest
+    }
+    
+    try request.session().data["name"] = Node.string(name)
+    
+    return "Remebered name."
+}
+
+drop.get("remember") { request in
+    guard let name = try request.session().data["name"]?.string else {
+        throw Abort.custom(status: .badRequest, message: "Please POST the name first.")
+    }
+    
+    return name
+}
 
 drop.run()
