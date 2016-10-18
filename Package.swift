c164aad48cbf4b9219059e0af834c517fff1db21
Mohammad Merajul Islam Molla <merajul.molla@sekai-lab-bd.com>
2016-10-18 11:46:33 +0600

    Remove extra ,

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ let package = Package(
     dependencies: [
         .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 1),
         .Package(url: "https://github.com/vapor/mysql-provider.git", majorVersion: 1, minor: 0),
-        .Package(url: "https://github.com/SwiftyBeaver/SwiftyBeaver-Vapor.git", majorVersion: 1),
+        .Package(url: "https://github.com/SwiftyBeaver/SwiftyBeaver-Vapor.git", majorVersion: 1)
     ],
     exclude: [
         "Config",
