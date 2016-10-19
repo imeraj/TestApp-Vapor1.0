6926bccb8fab2d7690ed23d360b44e83919a5bf1
Istiak Morsalin <anik.cse@gmail.com>
2016-10-19 17:24:07 +0600

    added formatting in Bird list table

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -6,22 +6,28 @@ import SwiftyBeaverVapor
 import SwiftyBeaver
 import Hash
 import Auth
+import Sessions
 
 // Initialize middlewares/providers
 let console = ConsoleDestination()
 let sbProvider = SwiftyBeaverProvider(destinations: [console])
 
-var middleware: [String: Middleware]? = [
-    "sighting-error": SightingErrorMiddleware(),
-    "user-error": UserErrorMiddleware(),
-    "auth-error": AuthErrorMiddleware(),
-    "auth": AuthMiddleware(user: User.self),
-    "logout": LogoutMiddleware(),
-    "validation-error": ValidationErrorMiddleware()
-]
-
 // Initialize Droplet
-let drop = Droplet(availableMiddleware: middleware, preparations: [Sighting.self, User.self], providers: [VaporMySQL.Provider.self], initializedProviders: [sbProvider])
+let drop = Droplet(preparations: [Sighting.self, User.self], providers: [VaporMySQL.Provider.self], initializedProviders: [sbProvider])
+
+// as workaround of framework bug, we add all middlewares here
+drop.addConfigurable(middleware: SessionsMiddleware(sessions: MemorySessions()), name: "sessions")
+drop.addConfigurable(middleware: AbortMiddleware(), name: "abort")
+drop.addConfigurable(middleware: DateMiddleware(), name: "date")
+drop.addConfigurable(middleware: TypeSafeErrorMiddleware(), name: "type-safe")
+drop.addConfigurable(middleware: ValidationMiddleware(), name: "validation")
+drop.addConfigurable(middleware: FileMiddleware(publicDir: drop.workDir + "Public/"), name: "file")
+drop.addConfigurable(middleware: SightingErrorMiddleware(), name: "sighting-error")
+drop.addConfigurable(middleware: UserErrorMiddleware(), name: "user-error")
+drop.addConfigurable(middleware: AuthMiddleware(user: User.self), name: "auth")
+drop.addConfigurable(middleware: ValidationErrorMiddleware(), name: "validation-error")
+drop.addConfigurable(middleware: LogoutMiddleware(), name: "logout")
+
 var log = drop.log.self
 
 if drop.environment == .production {
@@ -39,9 +45,11 @@ log.info("API registration done!")
 drop.get("/") { request in
     try drop.view.make("signup.html")
 }
+
 drop.get("/login") { request in
     try drop.view.make("login.html")
 }
+
 drop.get("/birds") { request in
     try drop.view.make("birds.html")
 }
